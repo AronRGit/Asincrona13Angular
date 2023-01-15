45ec759198bb73aae62b22df439fee96432941b7
AronRP <aron16d@gmail.com>
2023-01-15 11:32:17 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Actividad-13';
+  data: Array<any> = [
+    {
+      apellido: 'Arcila',
+      Casado: false,
+      Direccion: 'Calle 35 43',
+      Nombre: 'Juan',
+      Telefono: '1234567',
+    },
+    {
+      apellido: 'Bueno',
+      Casado: false,
+      Direccion: 'Calle 34353',
+      Nombre: 'Kevin',
+      Telefono: '43546',
+    },
+    {
+      apellido: 'Palomino',
+      Casado: false,
+      Direccion: 'Calle w323',
+      Nombre: 'Natalia',
+      Telefono: '4353423',
+    },
+  ];
 }
